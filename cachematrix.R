556fa2e777f952d7f0de19aeb48c126d38b1f14e
juandarnay <66964391+juandarnay@users.noreply.github.com>
2020-11-16 13:12:10 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,67 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        #  we declare the variable we want to obtain as a variable with a null value.
+        # declaramos la variable que queremos obtener como una variable con valor nulo.
+        inversa <- NULL
+        #  we declare the variable "set" that will contain a function that will receive an array and equal "x" with the value of "y", that is to say that when it is entered through that function we will be able to give a new value to the array that it already has.
+        #  declaramos la variable "set" que contendrá una funcion que recibirá una matriz e igualará a "x"con el valor de "y", es decir que cuando se ingrese a través de dicha función vamos a poder dar un nuevo valor a la matriz que ya se tiene.
+        set <- function(y) {
+                x <<- y
+               
+                #  we indicate that if we enter another array, our "inverse" variable has a null value.
+                #  indicamos que si ingresamos otra matriz, nuestra variable "inversa" tenga un valor nulo.
+                inversa <<- NULL
+        }
+       
+        # "get" is a function that will return the value of the array you have. 
+        #  "get" es una función que devolverá el valor de la matriz que se tenga.
+        get <- function() x
+        
+        # the function "setsolve" will give the value of the inverse calculated the variable "inverse".
+        #  la función "setsolve" dará el valor de la inversa calculada la variable "inversa".
+        setsolve <- function(inversacalculada) {inversa <<- inversacalculada}
+       
+        #  with this function we are going to observe the value of the calculated inverse. 
+        #  con esta función vamos a porder observar el valor de la inversa calculada.
+        getsolve <- function(){inversa}
+        
+        # we create a list matching the functions we have.
+        # creamos una lista igualando las funciones que tenemos.
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
-
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Return a matrix that is the inverse of 'x'
+        
+        # we acquire the value of the variable "inverse" through the function "x$getsolve.
+        #  adquirimos el valor de la variable "inversa" a traves de la función "x$getsolve".
+        inversa <- x$getsolve()
+        
+        # if the "inverse" variable was already calculated, we acquire that data so we don't have to calculate it again.
+        #  si la variable "inversa" ya fue calculada, adquirimos ese dato para no tener que calcularla nuevamente.
+        if(!is.null(inversa)) {
+                message("getting cached data")
+                return(inversa)
+        }
+        
+        # we create a data variable which equals the array "x" that we created at the beginning through the function "x$get".
+        #  creamos una variable de datos la cual igualamos a la matriz "x" que creamos al principio a traves de la función "x$get".
+        data <- x$get()
+        
+        #  we calculate the inverse of the matrix, through the function solve.
+        #  calculamos la inversa de la matriz, a traves de la función solve.
+        inversa <- solve(data, ...)
+        
+        # we establish that the value of "x" be the value of the variable "inverse".
+        #  establecemos que el valor de "x" serel valor de la variable "inversa"
+        x$setsolve(inversa)
+        
+        # finally, we bring the value of the inverse already calculated.
+        #  por ultimo, traemos el valor de la inversa ya calculada.
+        inversa
+        }
